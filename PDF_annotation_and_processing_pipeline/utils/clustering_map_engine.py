14a6a69750140b071c41bd28793ae849ce422e7b
Geo99pro <lolo@aluno.puc-rio.br>
2025-01-10 12:30:48 -0300

    Refactor ClusteringMapEngine to accept parameters in visualize_clusters method, improving flexibility and usability

--- PDF_annotation_and_processing_pipeline/utils/clustering_map_engine.py
+++ PDF_annotation_and_processing_pipeline/utils/clustering_map_engine.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import plotly.express as px
 
@@ -15,14 +16,20 @@ class ClusteringMapEngine:
         visualize_clusters(): This method visualizes the clusters of the best reduced embeddings in 2D space.
     """
 
-    def __init__(self, best_embeddings_df: pd.DataFrame, path_to_save_cluster_map: str):
-        self.best_embeddings_df = best_embeddings_df
-        self.path_to_save_cluster_map = path_to_save_cluster_map
+    def visualize_clusters(self,
+                        best_embeddings_df: pd.DataFrame,
+                        path_to_save_cluster_map: str):
+        """
+        This method visualizes the clusters of the best reduced embeddings in 2D space.
 
-    def visualize_clusters(self):
-        name = 'clusters_in_2D_space'
-        df = self.best_embeddings_df.drop('Pdf_page_name', axis=1)
+        Args:
+            best_embeddings_df (pd.DataFrame): A pandas DataFrame containing the best reduced embeddings and the labels of the clusters.
+            path_to_save_cluster_map (str): A string containing the path to save the plot of the clusters in 2D space.
+        """
+
+        df = best_embeddings_df.drop('Pdf_page_name', axis=1)
         fig = px.scatter(df, x=df.columns[0], y=df.columns[1], color='Cluster', hover_data=['Cluster'])
         fig.update_layout(title='Clusters in 2D space', template='plotly_dark')
-        fig.write_image(f'{self.path_to_save_cluster_map}{name}.png')
-        print(f'Dear User ! The plot has been saved in {self.path_to_save_cluster_map} with the name {name}.')
+        fig_path = os.path.join(path_to_save_cluster_map, 'clusters_in_2D_space.png')
+        fig.write_image(fig_path)
+        #print(f'Dear User ! The plot has been saved in {path_to_save_cluster_map} with the name {name}.')
