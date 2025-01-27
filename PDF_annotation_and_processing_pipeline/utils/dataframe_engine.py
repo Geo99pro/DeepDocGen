9592cb1bf59871bfef57336175b43501d16ebe4f
Geo99pro <lolo@aluno.puc-rio.br>
2025-01-27 18:32:32 -0300

    Refactor DataFrameEngine methods to improve parameter handling and encapsulation

--- PDF_annotation_and_processing_pipeline/utils/dataframe_engine.py
+++ PDF_annotation_and_processing_pipeline/utils/dataframe_engine.py
@@ -12,35 +12,35 @@ class DataFrameEngine:
     """
     This class handles DataFrame creation and cluster analysis for embeddings.
 
-    Attributes:
-        best_reduced_embeddings (np.ndarray): Reduced embeddings as a NumPy array.
-        kmeans_labels (np.ndarray): Cluster labels from K-means.
-        desired_columns_names (str): Base name for embedding columns.
-        embeddings_extracted (dict): Dictionary with extracted embedding names and values.
-        save_to_excel_format (bool): Whether to save the DataFrame in Excel format.
-        kwargs (dict): Additional arguments like the save path.
-
     Methods:
-        create_dataframe(): Creates a DataFrame with embeddings and cluster labels.
-        test_cosine_similarity(meta_data_folder): Analyzes cosine similarity within each cluster.
-        plot_distribution_of_PDF_pages_by_cluster(): Generates a distribution plot for clusters.
-        setup_folder(): Prepares a folder for saving files.
-        copy_image(): Copies images based on similarity thresholds.
+        - create_dataframe: Creates a DataFrame with embeddings and cluster labels.
     """
 
-    def __init__(self, best_reduced_embeddings: np.ndarray, kmeans_prediction: np.ndarray, desired_columns_names: str, embeddings_extracted: dict, save_to_excel_format: bool, **kwargs):
-        self.best_reduced_embeddings = best_reduced_embeddings
-        self.kmeans_labels = kmeans_prediction
-        self.desired_columns_names = desired_columns_names
-        self.embeddings_extracted = embeddings_extracted
-        self.save_to_excel_format = save_to_excel_format
-        self.kwargs = kwargs
+    def create_dataframe(self,
+                         best_reduced_embeddings: np.ndarray,
+                         kmeans_labels: np.ndarray,
+                         desired_columns_names: str,
+                         embeddings_extracted: dict,
+                         save_to_excel_format: bool,
+                         **kwargs) -> pd.DataFrame:
+        """
+        This method is responsible for creating a DataFrame with embeddings and cluster labels.
 
-    def create_dataframe(self):
-        embeddings_df = pd.DataFrame(self.best_reduced_embeddings, 
-                                     columns=[self.desired_columns_names + str(i) for i in range(self.best_reduced_embeddings.shape[1])])
-        embeddings_df['Pdf_page_name'] = self.embeddings_extracted['embeddings_names']
-        embeddings_df['Cluster'] = self.kmeans_labels
+        Args:
+            - best_reduced_embeddings (np.ndarray): Reduced embeddings as a NumPy array.
+            - kmeans_labels (np.ndarray): Cluster labels from K-means.
+            - desired_columns_names (str): Base name for embedding columns.
+            - embeddings_extracted (dict): Dictionary with extracted embedding names and values.
+            - save_to_excel_format (bool): Whether to save the DataFrame in Excel format.
+            - kwargs (dict): Additional arguments like the save path. 
+
+        Returns:
+            - embeddings_df (pd.DataFrame): DataFrame with embeddings and cluster labels.
+        """
+        embeddings_df = pd.DataFrame(best_reduced_embeddings, 
+                                     columns=[desired_columns_names + str(i) for i in range(best_reduced_embeddings.shape[1])])
+        embeddings_df['Pdf_page_name'] = embeddings_extracted['embeddings_names']
+        embeddings_df['Cluster'] = kmeans_labels
 
         # Plot cluster distribution
         group_count = embeddings_df['Cluster'].value_counts().to_dict()
@@ -53,28 +53,38 @@ class DataFrameEngine:
         plt.xlabel('Clusters')
         plt.ylabel('Counts')
         plt.title('Distribution of PDF pages by cluster')
-        plt.savefig(f'{self.kwargs.get("path_to_save_dataframe")}distribution_of_PDF_pages_by_cluster.png')
+        plt.savefig(f'{kwargs.get("path_to_save_dataframe")}distribution_of_PDF_pages_by_cluster.png')
 
         # Save DataFrame as Excel file
-        if self.save_to_excel_format:
-            output_path = os.path.join(self.kwargs.get("path_to_save_dataframe"), 'embeddings_dataframe.xlsx')
+        if save_to_excel_format:
+            output_path = os.path.join(kwargs.get("path_to_save_dataframe"), 'embeddings_dataframe.xlsx')
             embeddings_df.to_excel(output_path, index=False)
             print(f'Dear User ! The embeddings dataframe has been saved successfully on the excel format. Check it out at: {output_path}')
         return embeddings_df
 
-    def test_cosine_similarity(self, meta_data_folder: dict):
+    def test_cosine_similarity(self, 
+                               embeddings_extracted,
+                               kmeans_labels, 
+                               meta_data_folder: dict,
+                               **kwargs):
         """
         Analyzes cosine similarity within clusters and copies images to folders based on thresholds.
 
         Args:
-            meta_data_folder (dict): Metadata for folders containing clustered images.
+            - embeddings_extracted (dict): Dictionary with extracted embeddings names and values.
+            - kmeans_labels (np.ndarray): Cluster labels from K-means.
+            - meta_data_folder (dict): Metadata for folders containing clustered images.
+            - kwargs (dict): Additional arguments like the save path.
+
+        Returns:
+            None. However, it saves images to folders based on thresholds.
         """
 
-        embedding_df = pd.DataFrame(self.embeddings_extracted)
+        embedding_df = pd.DataFrame(embeddings_extracted)
         embedding_df.rename(columns={'embeddings_names': 'Pdf_page_name'}, inplace=True)
-        embedding_df["Cluster"] = self.kmeans_labels
+        embedding_df["Cluster"] = kmeans_labels
         
-        for cluster_label in np.unique(self.kmeans_labels):
+        for cluster_label in np.unique(kmeans_labels):
             #Filter the dataframe by cluster
             filtered_per_cluster = embedding_df[embedding_df['Cluster'] == cluster_label]
             pdf_names = filtered_per_cluster['Pdf_page_name'].values
@@ -96,7 +106,9 @@ class DataFrameEngine:
 
             #Plot the distribution of the PDF pages by cluster
             overall_mean = mean_similarity.mean()
-            self.plot_distribution_of_PDF_pages_by_cluster(mean_similarity, overall_mean, cluster_label)
+            self.plot_distribution_of_PDF_pages_by_cluster(mean_similarity,
+                                                           overall_mean,
+                                                           cluster_label)
 
             #Filter pages by thresholds
             page_below_threshold = mean_similarity[mean_similarity < lower_quantile]
@@ -104,7 +116,7 @@ class DataFrameEngine:
             page_between_threshold = mean_similarity[(mean_similarity > first_quantile) & (mean_similarity < third_quantile)]
             
             #Save the images that are above and below the threshold
-            save_path = self.kwargs.get("path_to_save_dataframe")
+            save_path = kwargs.get("path_to_save_dataframe")
             self.setup_folder(os.path.join(save_path, f'images_below_threshold_group_{cluster_label}'))
             self.setup_folder(os.path.join(save_path, f'image_upper_threshold_group_{cluster_label}'))
             self.setup_folder(os.path.join(save_path, f'image_between_threshold_group_{cluster_label}'))
@@ -117,7 +129,11 @@ class DataFrameEngine:
                     self.copy_image(page_above_threshold, os.path.join(save_path, f'image_upper_threshold_group_{cluster_label}'), each_images_path)
                     self.copy_image(page_between_threshold, os.path.join(save_path, f'image_between_threshold_group_{cluster_label}'), each_images_path)
 
-    def plot_distribution_of_PDF_pages_by_cluster(self, mean_similarity: pd.Series, overall_mean: float, cluster_label: int):
+    def plot_distribution_of_PDF_pages_by_cluster(self,
+                                                  mean_similarity: pd.Series, 
+                                                  overall_mean: float,
+                                                  cluster_label: int,
+                                                  **kwargs):
         """
         This method is responsible for plotting the distribution of the PDF pages by cluster.
 
@@ -138,7 +154,7 @@ class DataFrameEngine:
         plt.title(f'Cluster {cluster_label} - Cosine Similarity Distribution')
         plt.legend()
         plt.grid(axis='both')
-        plt.savefig(os.path.join(self.kwargs.get("path_to_save_dataframe"), f'cluster_{cluster_label}_similarity_distribution.png'))
+        plt.savefig(os.path.join(kwargs.get("path_to_save_dataframe"), f'cluster_{cluster_label}_similarity_distribution.png'))
         plt.close()
 
     def setup_folder(self, folder_path: str):
